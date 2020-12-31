d6493138a8d2c4894c4a7a5a97d7cea36e6122c6
Gordon Murray <gordon@gordonmurray.com>
2020-12-31 12:10:14 +0000

    Use a subnet from the list

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "webserver" {
   ami                    = var.ami
   instance_type          = "t3a.nano"
   vpc_security_group_ids = [aws_security_group.webserver_sg.id]
-  subnet_id              = var.subnet
+  subnet_id              = element(var.subnets, 1)
   key_name               = aws_key_pair.pem-key.id
   tags = {
     Name = "webserver"
@@ -13,7 +13,7 @@ resource "aws_instance" "proxysql" {
   ami                    = var.ami
   instance_type          = "t3a.nano"
   vpc_security_group_ids = [aws_security_group.proxysql_sg.id]
-  subnet_id              = var.subnet
+  subnet_id              = element(var.subnets, 1)
   key_name               = aws_key_pair.pem-key.id
   tags = {
     Name = "proxysql"
